7086f7eb21d2c1431cd6951233dcac0e9e1aa2e9
Mateusz Midor <mateusz>
2017-04-04 08:44:50 +0200

    Cleaned up static/dynamic list examples

--- DynamicLinkedList/dynamiclinkedlist.c
+++ DynamicLinkedList/dynamiclinkedlist.c
@@ -1,53 +1,52 @@
 /**
- * @file    hello.c
- * @author  Derek Molloy, modified Mateusz Midor
- * @date    4 April 2015, Nov 3, 2016
- * @version 0.1
- * @brief  An introductory "Hello World!" loadable kernel module (LKM) that can display a message
- * in the /var/log/kern.log file when the module is loaded and removed. The module can accept an
- * argument when it is loaded -- the name, which appears in the kernel log files.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
-
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+ *   @file: dynamiclinkedlist.c
+ *
+ *   @date: Apr 04, 2017
+ * @author: Mateusz Midor
+ */
+#include <linux/module.h>	// included for all kernel modules
+#include <linux/kernel.h>	// included for KERN_INFO
+#include <linux/init.h> 	// included for __init and __exit macros
 #include <linux/list.h>
-#include <linux/slab.h>
+#include <linux/slab.h>		// kmalloc, kfree
 
 
-// define and init list head
+// 1. define and init list head
 LIST_HEAD(country_capital_list);
 
-// define list element type
+// 2. define list element type
 struct country_capital {
     struct list_head list; // this makes country_capital valid linked-list element
-    char *country;
-    char *capital;
+    char country[20];
+    char capital[20];
 };
 
-
-// alloc and add elments to the list
+// 3. alloc and add elments to the list
 static void fill_list(void) {
-	// kmalloc - contiguous virtual addresses and physical addresses
-	// vmalloc - contiguous virtual addresses, physical contiguous or not - allocated page by page, better chance to succeed
-	struct country_capital *c1 = kmalloc(sizeof(struct country_capital), GFP_KERNEL);
-	struct country_capital *c2 = kmalloc(sizeof(struct country_capital), GFP_KERNEL);
-	struct country_capital *c3 = kmalloc(sizeof(struct country_capital), GFP_KERNEL);
+	const char* COUNTRIES[] = {"hiszpania", "wietnam", "peru"};
+	const char* CAPITALS[] = {"madryt", "hanoi", "lima"};
 
-	c1->country = "hiszpania";
-	c1->capital = "madryt";
-	c2->country = "wietnam";
-	c2->capital = "hanoi";
-	c3->country = "peru";
-	c3->capital = "lima";
-
-    list_add_tail(&c1->list, &country_capital_list);
-    list_add_tail(&c2->list, &country_capital_list);
-    list_add_tail(&c3->list, &country_capital_list);
+	int i;
+	struct country_capital *item;
+	for (i = 0; i < sizeof(COUNTRIES) / sizeof(COUNTRIES[0]); i++) {
+		// kmalloc - contiguous virtual addresses and physical addresses
+		// vmalloc - contiguous virtual addresses, physical contiguous or not - allocated page by page, better chance to succeed
+		item = kmalloc(sizeof(struct country_capital), GFP_KERNEL); // GFP_KERNEL is for ordinary kernel memory allocations
+		strncpy(item->country, COUNTRIES[i], sizeof(item->country));
+		strncpy(item->capital, CAPITALS[i], sizeof(item->capital));
+		list_add_tail(&item->list, &country_capital_list);
+	}
 }
 
-// cleanup the list and free it's elements
+// 4. print list elements
+static void print_list(void) {
+    struct country_capital *entry;
+
+    list_for_each_entry(entry, &country_capital_list, list)
+        printk(KERN_INFO "%s - %s\n", entry->country, entry->capital);
+}
+
+// 5. cleanup the list and free it's elements
 static void clean_list(void) {
     struct country_capital *entry;
 
@@ -59,13 +58,6 @@ static void clean_list(void) {
     }
 }
 
-// print list elements
-static void print_list(void) {
-    struct country_capital *entry;
-
-    list_for_each_entry(entry, &country_capital_list, list)
-        printk(KERN_INFO "%s - %s\n", entry->country, entry->capital);
-}
 
 // #######################################################################
 // HERE STARTS THE STANDARD MODULE CONFIG - SETUP - TEARDOWN
--- StaticLinkedList/staticlinkedlist.c
+++ StaticLinkedList/staticlinkedlist.c
@@ -1,56 +1,51 @@
 /**
- * @file    hello.c
- * @author  Derek Molloy, modified Mateusz Midor
- * @date    4 April 2015, Nov 3, 2016
- * @version 0.1
- * @brief  An introductory "Hello World!" loadable kernel module (LKM) that can display a message
- * in the /var/log/kern.log file when the module is loaded and removed. The module can accept an
- * argument when it is loaded -- the name, which appears in the kernel log files.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
-
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+ *   @file: staticlinkedlist.c
+ *
+ *   @date: Apr 03, 2017
+ * @author: Mateusz Midor
+ */
+#include <linux/module.h>	// included for all kernel modules
+#include <linux/kernel.h>	// included for KERN_INFO
+#include <linux/init.h> 	// included for __init and __exit macros
 #include <linux/list.h>
 
 
 
-// define and init list head
+// 1. define and init list head
 LIST_HEAD(country_capital_list);
 
-// define list element type
+// 2. define list element type
 struct country_capital {
     struct list_head list; // this makes country_capital valid linked-list element
     char *country;
     char *capital;
 };
 
-// define some list elements
+// 3. define some static list elements
 struct country_capital c1 = {
-    .country = "hiszpania",
-    .capital = "madryt"
+    .country = "portugalia",
+    .capital = "lizbona"
 };
 
 struct country_capital c2 = {
-    .country = "wietnam",
-    .capital = "phnompen"
+    .country = "kambodża",
+    .capital = "phnompenh"
 };
 
 struct country_capital c3 = {
-    .country = "peru",
-    .capital = "lima"
+    .country = "meksyk",
+    .capital = "tenochtitlan "
 };
 
 
-// add elments to the list
+// 4. add elments to the list
 static void fill_list(void) {
     list_add_tail(&c1.list, &country_capital_list);
     list_add_tail(&c2.list, &country_capital_list);
     list_add_tail(&c3.list, &country_capital_list);
 }
 
-// print list elements
+// 5. print list elements
 static void print_list(void) {
     struct country_capital *entry;
 
@@ -64,7 +59,7 @@ static void print_list(void) {
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mateusz Midor");
-MODULE_DESCRIPTION("A linux linked list example");
+MODULE_DESCRIPTION("A linux statically allocated linked list example");
 MODULE_VERSION("0.1");
 
 static int __init hello_init(void) {
